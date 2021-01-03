51d54bf867509edb160847bbff3576cd11a77259
Aaron Marais <its_me@aaronleem.co.za>
2021-01-04 00:19:51 +0200

    typings

--- src/container.js
+++ src/container.js
@@ -10,29 +10,14 @@ export default class Container {
     this.#core = new Core();
   }
 
-  /**
-   * Get a new Container instance
-   * @returns {Container}
-   */
   static getInstance() {
     return new Container();
   };
 
-  /**
-   * Get the amount of dependency bindings currently registered
-   * @returns {Number}
-   */
   get bindingLen() {
     return this.#core.bindingLen;
   }
 
-  /***
-   * Register a new dependency as a normal class
-   * @param alias The string alias representing the type
-   * @param dependency The type itself (eg: Widget.prototype)
-   * @param ctorArgAliases Constructor argument array of type aliases
-   * @returns void
-   */
   register(alias, dependency, ctorArgAliases = []) {
     try {
       this.#core.addBinding({
@@ -46,13 +31,6 @@ export default class Container {
     }
   };
 
-  /***
-   * Register a new dependency as a singleton class
-   * @param alias The string alias representing the type
-   * @param dependency The type itself (eg: Widget.prototype)
-   * @param ctorArgAliases Constructor argument array of type aliases
-   * @returns void
-   */
   registerSingleton(alias, dependency, ctorArgAliases = []) {
     try {
       this.#core.addBinding({
@@ -66,14 +44,6 @@ export default class Container {
     }
   };
 
-  /***
-   * Register a new dependency as a normal class with a factory method
-   * @param alias The string alias representing the type
-   * @param dependency The type itself (eg: Widget.prototype)
-   * @param factoryMethod The factory method used to create the instance
-   * @param ctorArgAliases Constructor argument array of type aliases
-   * @returns void
-   */
   registerFactory(alias, dependency, factoryMethod, ctorArgAliases = []) {
     try {
       this.#core.addBinding({
@@ -88,14 +58,6 @@ export default class Container {
     }
   };
 
-  /***
-   * Register a new dependency as a singleton class with a factory method
-   * @param alias The string alias representing the type
-   * @param dependency The type itself (eg: Widget.prototype)
-   * @param factoryMethod The factory method used to create the instance
-   * @param ctorArgAliases Constructor argument array of type aliases
-   * @returns void
-   */
   registerSingletonFactory(alias, dependency, factoryMethod, ctorArgAliases = []) {
     try {
       this.#core.addBinding({
@@ -110,11 +72,6 @@ export default class Container {
     }
   };
 
-  /***
-   * Resolve a dependency by its alias which was registered
-   * @param alias
-   * @returns dependency instance
-   */
   resolve(alias) {
     try {
       return this.#core.createInstance(alias);
---
+++ typings/index.d.ts
@@ -0,0 +1,55 @@
+import Container from "totally-di";
+
+declare module "totally-di" {
+  export default class Container {
+    /**
+     * Get a new Container instance
+     */
+    static getInstance(): Container
+
+    /**
+     * Get the amount of dependency bindings currently registered
+     */
+    get bindingLen(): Number
+
+    /***
+     * Register a new dependency as a normal class
+     * @param alias The string alias representing the type
+     * @param dependency The type itself (eg: Widget.prototype)
+     * @param ctorArgAliases Constructor argument array of type aliases
+     * @returns void
+     */
+    register(alias: String, dependency: any, ctorArgAliases: Array = []): void
+
+    /***
+     * Register a new dependency as a singleton class
+     * @param alias The string alias representing the type
+     * @param dependency The type itself (eg: Widget.prototype)
+     * @param ctorArgAliases Constructor argument array of type aliases
+     */
+    registerSingleton(alias: String, dependency: any, ctorArgAliases: Array = []): void
+
+    /***
+     * Register a new dependency as a normal class with a factory method
+     * @param alias The string alias representing the type
+     * @param dependency The type itself (eg: Widget.prototype)
+     * @param factoryMethod The factory method used to create the instance
+     * @param ctorArgAliases Constructor argument array of type aliases
+     */
+    registerFactory(alias: String, dependency: any, factoryMethod: String, ctorArgAliases: Array = []): void
+
+    /***
+     * Register a new dependency as a singleton class with a factory method
+     * @param alias The alias representing the type
+     * @param dependency The type itself (eg: Widget.prototype)
+     * @param factoryMethod The factory method used to create the instance
+     * @param ctorArgAliases Constructor argument array of type aliases
+     */
+    registerSingletonFactory(alias: String, dependency: any, factoryMethod: String, ctorArgAliases: Array = []): void
+
+    /***
+     * Resolve a dependency by its alias which was registered
+     */
+    resolve(alias: String): any
+  };
+}
